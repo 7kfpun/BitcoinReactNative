0c821f0637f69ccff482ff34360e35c31ad8e5eb
kf <7kfpun@gmail.com>
2016-08-08 17:09:51 +0800

    Add rate us

--- app/views/settings.js
+++ app/views/settings.js
@@ -9,6 +9,7 @@ import {
 } from 'react-native';
 
 // 3rd party libraries
+import { Actions } from 'react-native-router-flux';
 import { AdMobBanner } from 'react-native-admob';
 import {
   Cell,
@@ -22,6 +23,12 @@ import NavigationBar from 'react-native-navbar';
 import { config } from '../config';
 
 export default class Main extends React.Component {
+  onActionSelected(position) {
+    if (position === 0) {  // index of 'Done'
+      Actions.pop();
+    }
+  }
+
   renderToolbar() {
     if (Platform.OS === 'ios') {
       return (
@@ -29,6 +36,11 @@ export default class Main extends React.Component {
           statusBar={{tintColor: '#455A64', style: 'light-content'}}
           style={styles.navigatorBarIOS}
           title={{title: this.props.title, tintColor: 'white'}}
+          rightButton={{
+            title: 'Done',
+            tintColor: '#3CABDA',
+            handler: Actions.pop,
+          }}
         />
       );
     } else if (Platform.OS === 'android') {
@@ -37,6 +49,10 @@ export default class Main extends React.Component {
           style={styles.toolbar}
           title={this.props.title}
           titleColor="white"
+          actions={[
+            {title: 'Done', iconName: 'check', iconSize: 26, show: 'always'},
+          ]}
+          onActionSelected={(position) => this.onActionSelected(position)}
         />
       );
     }
@@ -58,6 +74,16 @@ export default class Main extends React.Component {
                   {text: 'OK', onPress: () => console.log('OK Pressed')},
                 ]
               )} />
+              <Cell cellstyle="RightDetail" title="Rate us!" onPress={() => {
+                if (Platform.OS === 'ios') {
+                  Linking.openURL('itms-apps://itunes.apple.com/app/id1123557731');
+                } else if (Platform.OS === 'android') {
+                  Linking.openURL('market://details?id=com.kfpun.bitcoin');
+                }}}
+              />
+            </Section>
+
+            <Section header="OTHERS">
               <Cell cellstyle="RightDetail" title="View More by This Developer" onPress={() => {
                 if (Platform.OS === 'ios') {
                   Linking.openURL('https://itunes.apple.com/us/developer/kf-pun/id1116896894');
@@ -80,7 +106,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#EEEEEE',
-    marginBottom: 50,
   },
   navigatorBarIOS: {
     backgroundColor: '#455A64',
