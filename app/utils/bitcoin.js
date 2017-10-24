1dc6c9b5043bf7d0a2a42c111bbbf91267af4195
kf <7kfpun@gmail.com>
2017-10-24 10:31:42 +0800

    Upgrade react native to 0.35.0

--- app/utils/bitcoin.js
+++ app/utils/bitcoin.js
@@ -1,5 +1,7 @@
 module.exports = function bitcoin(currency = 'usd') {
   const URL = `http://api.coindesk.com/v1/bpi/currentprice/${currency}`;
   console.log(URL);
-  return fetch(URL).then(res => res.json());
+  const req = new Request(URL);
+  console.log('req', req);
+  return fetch(req).then(res => res.json());
 };
